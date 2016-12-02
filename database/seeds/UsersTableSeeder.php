ec8ec307815bafdf028ee720a73bfa1d98ab1203
Luke Tanprasit <tanprasitp@hotmail.co.uk>
2016-12-02 14:53:02 +0000

    Updated the seeder file for emails and name

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,16 +13,13 @@ class UsersTableSeeder extends Seeder
     {
         $faker = Faker\Factory::create();
 
-        for ($i = 0; $i < 1000; $i++) {
+        for ($i = 0; $i < 10; $i++) {
         	App\User::create([
-        		'name' => $faker->name ($title = 'null'),
-        		'email' => $faker->name . "@email.com",
+        		'name' => $faker->name,
+        		'email' => $faker->email,
         		'password' => bcrypt('secret'),
-        		'jobTitle' => $faker->randomElement($array = array('developer', 'accounts manager', 'tech support')),
-        		'organisation_id' => 1,
+        		'job_title' => $faker->randomElement($array = array('developer', 'accounts manager', 'tech support')),
         		'department_id' => 1,
-        		'hairColour' => $faker->randomElement($array = array ('brown', 'black', 'blonde', 'ginger')),
-        		'brewChoice' => $faker->randomElement($array = array ('tea', 'coffee', 'mate', 'matcha', 'cocoa'))	
         		]);
         }
     }

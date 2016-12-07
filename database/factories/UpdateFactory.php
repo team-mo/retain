407a92232be74f887173edc0926cadfc891ab3ab
Jake Davies <jdavies.thfc@gmail.com>
2016-12-07 17:41:07 +0000

    random element nuissance!

--- database/factories/UpdateFactory.php
+++ database/factories/UpdateFactory.php
@@ -3,7 +3,6 @@
     $factory->define(App\Update::class, function (Faker\Generator $faker) {
 
         return [
-            'name' => $faker->randomElements('Admiral', 'Nokia', 'BT', 'Alcatel-Lucent'),
             'description' => $faker->paragraph()
         ];
     });

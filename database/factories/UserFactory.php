c828e85791e15af198ac134ffa4fd305f09781c4
Adam Sofokleous <adamcyp@gmail.com>
2016-12-08 09:30:18 +0000

    Changed functions to singular
    And passed arrays as parameters

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,6 +8,6 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
-        'job_title' => $faker->randomElement($array = array ('engineer','developer','scientist'))
+        'job_title' => $faker->randomElement(['engineer','developer','scientist'])
     ];
 });
